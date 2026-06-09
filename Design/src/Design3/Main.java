32514462d9d872e3c17d511e5aa68f583913fe9a
Sabarivel Ganesan <sabarivel.ganesan@avantorsciences.com>
2026-06-09 14:08:49 +0530

    new changes

--- Design/src/Design3/Main.java
+++ Design/src/Design3/Main.java
@@ -82,86 +82,94 @@ public class Main {
     }
 
     static Payment payMent(Scanner scanner, long totalAmount) {
-        IO.print("\nSelect the payment method : \n");
-        IO.print("""
-                1.UPI
-                2.Card
-                3.Net Banking
-                """);
-        IO.print("Enter the payment method number for pay your bill amount " + totalAmount + " : ");
-        return new Payment(scanner.nextLine());
+        try {
+            IO.print("\nSelect the payment method : \n");
+            IO.print("""
+                    1.UPI
+                    2.Card
+                    3.Net Banking
+                    """);
+            IO.print("Enter the payment method number for pay your bill amount " + totalAmount + " : ");
+            return new Payment(scanner.nextLine());
+        }
+        catch (Exception e) {
+            IO.println("Please enter valid input");
+            return null;
+        }
     }
 
     static void start(Scanner scanner, Details d1) {
-        IO.print("Select the vehicle type based on the list\n");
-        IO.print("""
-                1.Bike
-                2.Car
-                3.Truck
-                """);
-        IO.print("Choose your vehicle type : ");
-        Vector<Vector<Boolean>>f;
-        boolean c = false;
-        int value = scanner.nextInt();scanner.nextLine();
-        switch (value) {
-            case 1:
-                f = Floors.getFloors();
-                for(int i=0;i<5 && !c;i++) {
-                    for(int j=0;j<20;j++) {
-                        if(!f.get(i).get(j)) {
-                            f.get(i).set(j,true);
-                            bike(scanner, d1, i, j, j);
-                            c = true;
-                            break;
-                        }
-                    }
-                }
-                if(!c) message();
-                break;
-            case 2:
-                f = Floors.getFloors();
-                for(int i=0;i<5 && !c;i++) {
-                    int j = 0;
-                    while(j<20) {
-                        if(!f.get(i).get(j)) {
-                            if (j + 2 < 20 && !f.get(i).get(j + 1) && !f.get(i).get(j + 2)) {
+        try {
+            IO.print("Select the vehicle type based on the list\n");
+            IO.print("""
+                    1.Bike
+                    2.Car
+                    3.Truck
+                    """);
+            IO.print("Choose your vehicle type : ");
+            Vector<Vector<Boolean>> f;
+            boolean c = false;
+            int value = scanner.nextInt();
+            scanner.nextLine();
+            switch (value) {
+                case 1:
+                    f = Floors.getFloors();
+                    for (int i = 0; i < 5 && !c; i++) {
+                        for (int j = 0; j < 20; j++) {
+                            if (!f.get(i).get(j)) {
                                 f.get(i).set(j, true);
-                                f.get(i).set(j + 1, true);
-                                f.get(i).set(j + 2, true);
-                                car(scanner, d1, i, j, j+2);
+                                bike(scanner, d1, i, j, j);
                                 c = true;
                                 break;
                             }
-                            else j += 3;
                         }
-                        else j++;
                     }
-                }
-                if(!c) message();
-                break;
-            case 3:
-                f = Floors.getFloors();
-                for(int i=0;i<5 && !c;i++) {
-                    int j = 0;
-                    while(j<20) {
-                        if(!f.get(i).get(j)) {
-                            if (j + 4 < 20 && !f.get(i).get(j + 1) && !f.get(i).get(j + 2) && !f.get(i).get(j + 3) && !f.get(i).get(j + 4)) {
-                                f.get(i).set(j, true);
-                                f.get(i).set(j + 1, true);
-                                f.get(i).set(j + 2, true);
-                                f.get(i).set(j + 3, true);
-                                f.get(i).set(j + 4, true);
-                                truck(scanner, d1, i, j, j+4);
-                                c = true;
-                                break;
-                            }
-                            else j += 5;
+                    if (!c) message();
+                    break;
+                case 2:
+                    f = Floors.getFloors();
+                    for (int i = 0; i < 5 && !c; i++) {
+                        int j = 0;
+                        while (j < 20) {
+                            if (!f.get(i).get(j)) {
+                                if (j + 2 < 20 && !f.get(i).get(j + 1) && !f.get(i).get(j + 2)) {
+                                    f.get(i).set(j, true);
+                                    f.get(i).set(j + 1, true);
+                                    f.get(i).set(j + 2, true);
+                                    car(scanner, d1, i, j, j + 2);
+                                    c = true;
+                                    break;
+                                } else j += 3;
+                            } else j++;
                         }
-                        else j++;
                     }
-                }
-                if(!c) message();
-                break;
+                    if (!c) message();
+                    break;
+                case 3:
+                    f = Floors.getFloors();
+                    for (int i = 0; i < 5 && !c; i++) {
+                        int j = 0;
+                        while (j < 20) {
+                            if (!f.get(i).get(j)) {
+                                if (j + 4 < 20 && !f.get(i).get(j + 1) && !f.get(i).get(j + 2) && !f.get(i).get(j + 3) && !f.get(i).get(j + 4)) {
+                                    f.get(i).set(j, true);
+                                    f.get(i).set(j + 1, true);
+                                    f.get(i).set(j + 2, true);
+                                    f.get(i).set(j + 3, true);
+                                    f.get(i).set(j + 4, true);
+                                    truck(scanner, d1, i, j, j + 4);
+                                    c = true;
+                                    break;
+                                } else j += 5;
+                            } else j++;
+                        }
+                    }
+                    if (!c) message();
+                    break;
+            }
+        }
+        catch (Exception e) {
+            IO.println("Please enter valid input");
         }
     }
 
@@ -186,61 +194,74 @@ public class Main {
             printBill(num, vd);
             return false;
         }
-        IO.print("\nIf you want to generate your bill, please enter 1 else 2 : ");
-        if(scanner.nextInt()==2) return false;
-        vd.getVehicle().setOutTime(out);
-        print(num, vd.getVehicle());
-        IO.println("Out Time : " + vd.getVehicle().getOutTime().format(value));
-        checkOut(vd.getVehicle().getFloor(), vd.getVehicle().getStart(), vd.getVehicle().getEnd());
-        Bill bill = new Bill();
-        bill.setTotalTime(totalTime);
-        bill.calculateBill(vd.getVehicle().getSize());
-        bill.setStatus(bill.getTotalAmount() == 0);
-        vd.setBill(bill);
-        printBill(num, vd);
-        if(vd.getBill().getStatus()) return false;
-        IO.print("Press 1 to pay the bill or press 2 for pay later : ");
-        if(scanner.nextInt()==2) return false;
-        scanner.nextLine();
-        Payment payment = payMent(scanner, vd.getBill().getTotalAmount());
-        IO.print("\n");
-        vd.setPayment(payment);
-        vd.getBill().setStatus(payment.getStatueforTID());
-        IO.println("\nYou successfully done the payment");
-        IO.println("Your payment type : " + vd.getPayment().getPaymentType());
-        IO.println("Your transaction id : " + vd.getPayment().getTransactionId());
-        return true;
+        try {
+            IO.print("\nIf you want to generate your bill, please enter 1 else 2 : ");
+            if (scanner.nextInt() == 2) return false;
+            vd.getVehicle().setOutTime(out);
+            print(num, vd.getVehicle());
+            IO.println("Out Time : " + vd.getVehicle().getOutTime().format(value));
+            checkOut(vd.getVehicle().getFloor(), vd.getVehicle().getStart(), vd.getVehicle().getEnd());
+            Bill bill = new Bill();
+            bill.setTotalTime(totalTime);
+            bill.calculateBill(vd.getVehicle().getSize());
+            bill.setStatus(bill.getTotalAmount() == 0);
+            vd.setBill(bill);
+            printBill(num, vd);
+            if (vd.getBill().getStatus()) return false;
+            IO.print("Press 1 to pay the bill or press 2 for pay later : ");
+            if (scanner.nextInt() == 2) return false;
+            scanner.nextLine();
+            Payment payment = payMent(scanner, vd.getBill().getTotalAmount());
+            if(payment==null) return false;
+            IO.print("\n");
+            vd.setPayment(payment);
+            vd.getBill().setStatus(payment.getStatueforTID());
+            IO.println("\nYou successfully done the payment");
+            IO.println("Your payment type : " + vd.getPayment().getPaymentType());
+            IO.println("Your transaction id : " + vd.getPayment().getTransactionId());
+            return true;
+        }
+        catch (Exception e) {
+            IO.println("Please enter valid input");
+            return false;
+        }
     }
 
     static void main() {
         Floors.init();
         Details d1 = new Details();
         Scanner scanner = new Scanner(System.in);
-        IO.println("Welcome choose any functions base on the list :");
-        while(true) {
-            IO.print("""
-                     1.Start the application
-                     2.View your parked vehicle details
-                     3.Check-out vehicle and generate bill
-                     4.Exit
-                     """);
-            IO.print("Enter the function number : ");
-            int v = scanner.nextInt();
-            scanner.nextLine();
-            if (v == 1) start(scanner, d1);
-            else if(v == 2) view(scanner, value);
-            else if(v == 3) {
-                if(bill(scanner, "", value)) {
-                    IO.print("\nThanks for you time, have a great day");
+        try {
+            IO.println("Welcome choose any functions base on the list :");
+            while (true) {
+                IO.print("""
+                        1.Start the application
+                        2.View your parked vehicle details
+                        3.Check-out vehicle and generate bill
+                        4.Exit
+                        """);
+                IO.print("Enter the function number : ");
+                int v = scanner.nextInt();
+                scanner.nextLine();
+                if (v == 1) start(scanner, d1);
+                else if (v == 2) view(scanner, value);
+                else if (v == 3) {
+                    if (bill(scanner, "", value)) {
+                        IO.print("\nThanks for you time, have a great day");
+                        break;
+                    }
+                } else {
                     break;
                 }
             }
-            else {
-                IO.print("\nThanks for you time, have a great day");
-                break;
-            }
+            IO.print("\n");
+            IO.print("\n\n");
         }
-        IO.print("\n");
-        IO.print("\n\n");
+        catch (Exception e) {
+            IO.println("Please enter valid input");
+        }
+        finally {
+            IO.print("\nThanks for you time, have a great day");
+        }
     }
 }
